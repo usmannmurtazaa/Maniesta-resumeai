2454b0cbbc13c9b1ec3def7bd965b13f7a42de43
Usman Murtaza <usmanmurtaza2004@gmail.com>
2026-08-30 01:01:00 +0500

    Add redesigned landing, auth pages, and job system

--- src/pages/AdminUserDetailPage.tsx
+++ src/pages/AdminUserDetailPage.tsx
@@ -1,15 +1,13 @@
-import { useParams } from 'react-router-dom';
 import { PageTransition } from '@/components/common/PageTransition';
 import { BackButton } from '@/components/common/BackButton';
 import { UserDetail } from '@/components/admin/users/UserDetail';
 
 export default function AdminUserDetailPage() {
-  const { userId } = useParams<{ userId: string }>();
   return (
     <PageTransition>
       <div>
         <BackButton label="Back to Users" to="/admin/users" />
-        <UserDetail userId={userId} />
+        <UserDetail />
       </div>
     </PageTransition>
   );
